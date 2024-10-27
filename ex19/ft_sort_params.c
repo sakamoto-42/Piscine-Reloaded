5c08671ce9ba714606487f0db7d1f3daf8392a23
julien <julien66140@hotmail.fr>
2024-10-27 11:02:18 +0100

    fix sort params

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,40 +6,31 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 10:23:42 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/10/27 10:46:14 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/10/27 11:00:31 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	ft_putstr(char *str)
-{
-	while (*str)
-	{
-		ft_putchar(*str);
-		str++;
-	}
-}
-
-void	ft_swap_str(char *s1, char *s2)
+void	ft_swap_str(char **s1, char **s2)
 {
 	char	*temp;
 
-	temp = s1;
-	s1 = s2;
-	s2 = temp;
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
 }
 
-int		ft_must_swap_str(char *s1, char *s2)
+int	ft_must_swap_str(char *s1, char *s2)
 {
-	while (*s1 || *s2)
+	while (*s1 && *s2)
 	{
-		if (*s1 > *s2)
-			return (1);
+		if (*s1 != *s2)
+			return (*s1 > *s2);
 		s1++;
 		s2++;
 	}
-	return (0);
+	return (*s1 > *s2);
 }
 
 void	ft_sort_params(int size, char **tabstr)
@@ -54,7 +45,7 @@ void	ft_sort_params(int size, char **tabstr)
 		while (j < size)
 		{
 			if (ft_must_swap_str(tabstr[i], tabstr[j]))
-				ft_swap_str(tabstr[i], tabstr[j]);
+				ft_swap_str(&tabstr[i], &tabstr[j]);
 			j++;
 		}
 		i++;
@@ -68,7 +59,12 @@ void	ft_print_params(int size, char **tabstr)
 	i = 1;
 	while (i < size)
 	{
-		ft_putstr(tabstr[i]);
+		while (*tabstr[i])
+		{
+			ft_putchar(*tabstr[i]);
+			tabstr[i]++;
+		}
+		ft_putchar('\n');
 		i++;
 	}
 }
